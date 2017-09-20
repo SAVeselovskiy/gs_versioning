25a35ee5eda50f09dbf8c02a2e51c1840d96780a
Сергей Веселовский <sergejveselovskij@MacBook-Pro-Sergej.local>
2017-09-20 16:26:32 +0300

    Merge branch 'master' of https://github.com/SAVeselovskiy/gs_versioning
    # Conflicts:
    #	LICENSE
    #	README.md

--- lib/fastlane/plugin/gs_versioning/actions/gs_increment_rc_version.rb
+++ lib/fastlane/plugin/gs_versioning/actions/gs_increment_rc_version.rb
@@ -14,14 +14,14 @@ module Fastlane
         if major > v["rc"].major
           v["rc"].major = major
           v["rc"].minor = 0
-          v["rc"].build = 0
+          v["rc"].build = 1
         elsif major < v["rc"].major
           raise "Wrong major number specified in Info.plist. Version major number can't be less than current major number on app store (and versions.json file)"
         elsif v["release"].major < v["rc"].major || v["release"].minor < v["rc"].minor
           v["rc"].build += 1
         else
           v["rc"].minor +=1
-          v["rc"].build = 0
+          v["rc"].build = 1
         end
         res = v["rc"].toString
         UI.message("New relese_candidate version " + res)
